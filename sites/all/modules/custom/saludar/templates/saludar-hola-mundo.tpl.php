952492b74407453350bd8193bae5d8455225891e
carlos-diaz-bbva <carlosmanuel.diaz.jorge@bbva.com>
2014-11-19 21:05:31 +0100

    Iniciamos con plantilla vacia

--- sites/all/modules/custom/saludar/templates/saludar-hola-mundo.tpl.php
+++ sites/all/modules/custom/saludar/templates/saludar-hola-mundo.tpl.php
@@ -1,15 +1,5 @@
 <div class='saludar'>
-	<div class="saludo"><em><?php print render($saludo); ?></em></div>
-	<?php print render($fecha); ?>
-	<div class="username">
-		<b>
-			<?php
-			if (isset($usuario->field_nombre)) {
-				print "Hasta pronto, " . render($usuario->field_nombre['und'][0]['value']) . "!";
-			}
-			?>
-		</b>
-	</div>
+
 </div>
 
 
